65997c25ef59cb4b7f9e061b4458327cf7867269
Tony Mobily <tonymobily@gmail.com>
2016-07-22 10:06:46 +0800

    First commit, plain element template

---
+++ make_readme.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+> README.md
+
+IFS=''
+active='0';
+cat  hot-form.html | while read -r "line";do
+
+
+  echo $line | grep '\-\->' > /dev/null
+  if [ $active = '1' -a $? = '0' ];then 
+    exit 0;
+  fi;
+
+  suppress=0;
+  echo $line | grep '^ *@' > /dev/null
+  if [ $? = '0' ];then suppress='1'; fi;
+  
+
+  if [ $active = '1' -a $suppress = '0' ];then echo $line >> README.md;fi;
+
+  echo $line | grep "<!--" > /dev/null
+  if [ $? = '0' ];then active='1'; fi;
+
+done
